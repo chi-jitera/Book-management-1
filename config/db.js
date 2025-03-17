d5925eaf06996acf366798397943c1eaa30d3955
Evey (Jitera) <developers@jitera.com>
2025-03-17 18:32:05 +0000

    feat[database]: delete tables 'book' Changes + '/config/db.js': deleted + '/server.js': deleted

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/bookstore', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('MongoDB Connection Error:', err));
-
-module.exports = mongoose;
