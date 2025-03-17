9bbdcdfd12d4f4373598809df0f85b83964879c7
Diane-marie (Jitera) <developers@jitera.com>
2025-03-17 14:58:12 +0000

    feat[database]: delete tables 'book', 'tests' Changes + '/controllers/bookController.js': deleted + '/models/Book.js': deleted + '/routes/bookRoutes.js': deleted + '/services/bookService.js': deleted + '/models/Test.js': deleted + '/config/migrations/1742223405590_create_tests_table.js': deleted

--- models/Test.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../config/db');
-
-const testSchema = new mongoose.Schema({
-    id: { type: Number, required: true, unique: true, auto: true },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Test', testSchema);
