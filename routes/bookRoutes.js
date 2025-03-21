a471b32281caa7b4bf1d6a17c9219869db956e27
Cortney (Jitera) <ai_developers@jitera.com>
2025-03-21 16:20:30 +0000

    feat: add PATCH support to books update endpoint

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/books', bookController.createBook);
 router.get('/books', bookController.getAllBooks);
 router.get('/books/:id', bookController.getBookById);
+router.patch('/books/:id', bookController.updateBook);
 router.put('/books/:id', bookController.updateBook);
 router.delete('/books/:id', bookController.deleteBook);
 
