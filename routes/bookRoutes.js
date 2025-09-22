b3dff24da67e7e8728f6124b1320c9fb8b15e325
Maxie (Jitera) <ai_developers@jitera.com>
2025-09-22 09:39:51 +0000

    feat: add PATCH /books/:id route for partial updates

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,6 +8,8 @@ router.get('/books', bookController.getAllBooks);
 router.get('/books/search', bookController.searchBooks);
 router.get('/books/:id', bookController.getBookById);
 router.put('/books/:id', bookController.updateBook);
+router.patch('/books/:id', bookController.updateBookPartial); // <--- PATCH route added
+
 router.delete('/books/:id', bookController.deleteBook);
 
 module.exports = router;
