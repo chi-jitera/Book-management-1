d0c9d956ceeab3dc8a5b5ba0301052a8f8c08232
Chi Le Thi Mai <chi.le@jitera.com>
2025-12-17 22:41:53 +0700

    Revert "Implement Services Core Module for Book and Review Management"

--- services/reviewService.js
+++ services/reviewService.js
@@ -1,14 +1,5 @@
 const Review = require('../models/Review');
 
-/**
- * Get all reviews for a specific book
- * @param {string} bookId
- * @returns {Promise<Array>} Array of Review documents
- */
-const getReviewsByBookId = async (bookId) => {
+exports.getReviewsByBookId = async (bookId) => {
     return await Review.find({ book: bookId });
-};
-
-module.exports = {
-    getReviewsByBookId
 };
